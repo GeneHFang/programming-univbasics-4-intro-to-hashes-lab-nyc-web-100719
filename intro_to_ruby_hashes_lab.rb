df985292e3c09da442148b3fd1b4a6a5ee2b19a4
Gene H Li <ghl234@nyu.edu>
2019-09-17 23:39:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,27 @@
 def new_hash
   # return an empty hash
+  empty = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  valid = {
+    "a" = "a"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  grace = {
+    name: "Grace Hopper"
+  }
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  pos = {
+    id: 5
+  }
 end
 
 def my_hash_creator(key, value)
